8d49835b57e6098123a260d7870216dafbeabe85
mironova2056 <145144059+mironova2056@users.noreply.github.com>
2025-03-15 21:48:08 +0700

    column component

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-
+Vue.component('column', {
+    props: {
+        column:{
+            type:Object,
+            required:true
+        },
+        columnIndex:{
+          type:Number,
+          required:true
+        },
+        isFirstColumnBlock:{
+            type:Boolean,
+            default:false
+        }
+    },
+    template: `
+        <div class="column">
+            <h2>{{column.title}} ({{column.cards.length}})</h2>
+        </div>
+    `,
+    computed: {
+    },
+})
